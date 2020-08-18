56afddc9393fc0b2a649d03689156fd6830a9f86
Ross Perry <perryr16@gmail.com>
2020-08-18 13:30:38 -0600

    begin annotating api/build/config

--- code/api/src/migrations/1-user.js
+++ code/api/src/migrations/1-user.js
@@ -1,3 +1,6 @@
+// a migration for a user. Each attribute has specific options defined.
+// Mostly datatypes of each attribute/column. 
+// ex: name = string
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('users', {
@@ -29,6 +32,7 @@ module.exports = {
       }
     });
   },
+  // how do you specify if you want to run the 'down' portion?
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('users');
   }
