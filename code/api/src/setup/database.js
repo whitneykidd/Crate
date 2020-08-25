cba1390b2ba7169d5178fef5dfcb055f9cbbf6ec
Whitney Kidd <whitneymkidd@gmail.com>
2020-08-25 16:23:04 -0600

    comment out connection

--- code/api/src/setup/database.js
+++ code/api/src/setup/database.js
@@ -18,13 +18,13 @@ const connection = new Sequelize(databaseConfigEnv.database, databaseConfigEnv.u
 // Test connection
 console.info('SETUP - Connecting database...')
 
-connection
-  .authenticate()
-  .then(() => {
-    console.info('INFO - Database connected.')
-  })
-  .catch(err => {
-    console.error('ERROR - Unable to connect to the database:', err)
-  })
+// connection
+//   .authenticate()
+//   .then(() => {
+//     console.info('INFO - Database connected.')
+//   })
+//   .catch(err => {
+//     console.error('ERROR - Unable to connect to the database:', err)
+//   })
 
 export default connection
