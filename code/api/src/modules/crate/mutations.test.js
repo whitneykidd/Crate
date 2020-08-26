25140fa25830a0f24beec0d029beee3fca067317
Ross Perry <perryr16@gmail.com>
2020-08-26 16:50:06 -0600

    modify setup to unseed, queries tested

--- code/api/src/modules/crate/mutations.test.js
+++ code/api/src/modules/crate/mutations.test.js
@@ -7,7 +7,7 @@ import graphqlHTTP from 'express-graphql'
 describe('crate mutations', () => {
   let server;
 
-  beforeAll(() => { // get the server running and attached for each of our future tests
+  beforeAll( async () => { // get the server running and attached for each of our future tests
     server = express();
 
     server.use(
@@ -17,16 +17,28 @@ describe('crate mutations', () => {
         graphiql: true, // use the graphiql feature
       })
     )
+    // const response = await request(server)
+    //   .get('/')
+    //   .send({query: `{userLogin(email:"admin@crate.com", password: "123456"){
+    //     token
+    //   }}`})
+    //   .expect(200)
+
+      // console.log(auth.user)
   })
 
   it('is true', () => {
     expect(true).toBe(true)
   })
 
-  it('returns crates and crate given an ID', async () => {
-    // need to figure out how to authenticate a user before doing 
-    // ANYTHING to a crate
+  // it('returns crates and crate given an ID', async () => {
+  //   const response = await request(server)
+  //     .get('/')
+  //     .send({query: `mutation {crateCreate(name: "c1", description: "d1"){name}}`})
+  //     .expect(200)
 
-  })
+  //     console.log(response.body.data.crateCreate)
+
+  // })
 
 })
--- code/api/src/modules/crate/query.test.js
+++ code/api/src/modules/crate/query.test.js
@@ -2,12 +2,14 @@ import request from 'supertest'
 import express from 'express'
 import schema from '../../setup/schema'
 import graphqlHTTP from 'express-graphql'
+import models from '../../setup/models'
+import { dedentBlockStringValue } from 'graphql/language/blockString'
 
 
 describe('crate mutations', () => {
   let server;
 
-  beforeAll(() => { // get the server running and attached for each of our future tests
+  beforeAll( async () => { // get the server running and attached for each of our future tests
     server = express();
 
     server.use(
@@ -19,9 +21,29 @@ describe('crate mutations', () => {
     )
   })
 
-  it('is true', () => {
-    expect(true).toBe(true)
+  beforeEach( async () => {
+    const crate1 = {
+      name: "crate1",
+      description: "description1"
+    }
+
+    const crate2 = {
+      name: "crate2",
+      description: "description2"
+    }
+
+    await models.Crate.create(crate1);
+    await models.Crate.create(crate2);
   })
+
+  afterEach( async () => {
+    await models.Crate.destroy({ where: {name: ["crate1", "crate2"]} })
+  })
+
+  afterAll( async () => {
+    db.close();
+  })
+
 
   it('returns crates and crate given an ID', async () => {
     const responseAll = await request(server)
@@ -29,19 +51,21 @@ describe('crate mutations', () => {
       .send({ query: `{crates (orderBy: "asc") {id name description}}`})
       .expect(200)
 
-      expect(responseAll.body.data.crates.length >= 6).toBe(true)
-      expect(responseAll.body.data.crates[0].name).toEqual("Clothes for Men")
-      expect(responseAll.body.data.crates[0].description).toEqual("A monthly supply of trendy clothes for men.")
-      let crateId = responseAll.body.data.crates[0].id
+      expect(responseAll.body.data.crates.length >= 2).toBe(true)
+      let length = responseAll.body.data.crates.length
 
-    const responseOne = await request(server)
+      expect(responseAll.body.data.crates[length-1].name).toEqual("crate2")
+      expect(responseAll.body.data.crates[length-1].description).toEqual("description2")
+      let crateId = responseAll.body.data.crates[length-1].id
+
+    const responseId = await request(server)
       .get('/')
-      .send({ query: `{crateById (crateId: ${crateId}) {id name description}}`})
+      .send({query: `{crateById(crateId: ${crateId}){ id name description}}`})
       .expect(200)
 
-    expect(responseOne.body.data.crateById.name).toEqual("Clothes for Men")
-    expect(responseOne.body.data.crateById.description).toEqual("A monthly supply of trendy clothes for men.")
-    expect(responseOne.body.data.crateById.id).toEqual(crateId)
+      expect(responseId.body.data.crateById.id).toEqual(crateId)
+      expect(responseId.body.data.crateById.name).toEqual("crate2")
+      expect(responseId.body.data.crateById.description).toEqual("description2")
   })
 
 })
