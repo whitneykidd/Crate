b9b8005662458a104fd7f27e419a4796e8efeab5
Ross Perry <perryr16@gmail.com>
2020-08-26 16:50:06 -0600

    modify setup to unseed, queries tested

--- code/api/src/modules/product/query.test.js
+++ code/api/src/modules/product/query.test.js
@@ -2,12 +2,13 @@ import request from 'supertest'
 import express from 'express'
 import schema from '../../setup/schema'
 import graphqlHTTP from 'express-graphql'
+import models from '../../setup/models'
 
 
 describe('product quereis', () => {
   let server;
 
-  beforeAll(() => { // get the server running and attached for each of our future tests
+  beforeAll( async () => { // get the server running and attached for each of our future tests
     server = express();
 
     server.use(
@@ -19,8 +20,33 @@ describe('product quereis', () => {
     )
   })
 
-  it('is true', () => {
-    expect(true).toBe(true)
+  beforeEach( async () => {
+    const product1 = {
+      name: "name1",
+      slug: "slug1",
+      type: 1,
+      gender: 1,
+      description: "desc1"
+    }
+
+    const product2 = {
+      name: "name2",
+      slug: "slug2",
+      type: 2,
+      gender: 2,
+      description: "desc2"
+    }
+
+    await models.Product.create(product1)
+    await models.Product.create(product2)
+  })
+
+  afterEach( async () => {
+    await models.Product.destroy({ where: { name: ["name1", "name2"] } });
+  })
+
+  afterAll(() => {
+    db.close();
   })
 
   it('returns all products and products by ID', async () => {
@@ -29,10 +55,24 @@ describe('product quereis', () => {
       .send({ query: `{products {id name slug type gender description }}`})
       .expect(200)
 
-    expect(responseAll.body.data.products.length <= 8).toBe(true)
-    expect(responseAll.body.data.products[0].name).toEqual('T-Shirt for Men - Grey')
-    expect(responseAll.body.data.products[0].slug).toEqual('t-shirt-for-men-grey')
+    expect(responseAll.body.data.products.length >= 2).toBe(true)
+
+    expect(responseAll.body.data.products[0].name).toEqual("name2")
+    expect(responseAll.body.data.products[0].slug).toEqual("slug2")
+    expect(responseAll.body.data.products[0].type).toEqual(2)
+    expect(responseAll.body.data.products[0].gender).toEqual(2)
+    expect(responseAll.body.data.products[0].description).toEqual("desc2")
+    let productId = responseAll.body.data.products[0].id
+
+    const responseOne = await request(server)
+      .get('/')
+      .send({query: `{productById(productId: ${productId}){id name }}`})
+      .expect(200)
+
+      expect(responseOne.body.data.productById.name).toEqual("name2")
+      expect(responseOne.body.data.productById.id).toEqual(productId)
   })
+  //'t-shirt-for-men-grey'
   // it('returns crates and crate given an ID', async () => {
   //   const responseAll = await request(server)
   //     .get('/')
