0b65f27f822adf8ac4bb5678ee71c6f5b054b8e3
Jeremiah Black <jeremiahblackol@gmail.com>
2020-08-26 18:28:19 -0600

    button clicks are now reflected in component state

--- code/web/src/modules/styleSurvey/StyleSurvey.js
+++ code/web/src/modules/styleSurvey/StyleSurvey.js
@@ -1,7 +1,8 @@
 // Imports
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+// import e from 'express'
+// import { connect } from 'react-redux'
 
 // App Imports
 // import { someAction } from './api/actions'
@@ -12,32 +13,83 @@ class StyleSurvey extends PureComponent {
   constructor(props) {
     super(props)
 
-    this.state = {}
+    this.state = {
+      tops: [],
+      bottoms: [],
+      dresses: [],
+      shoes: [],
+      accessories: []
+    }
   }
 
   componentDidMount() {
     // this.props.someAction()
   }
 
+  handleChange(event) {
+    event.preventDefault()
+    // 
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  // make a function that will make the section for us
+
+  generateButtonSections = (name) => {
+    // for each image return one of the inputs
+    // and the name of the input will be name arg
+  }
+
+
+
   render() {
     return (
-      <div>
+      <form>
         <h1>Style Survey</h1>
-        <div>
-            <h2>Tops</h2>
-        </div>
-        <button>HELLO</button>
-        <button>WHATS UP</button>
-      </div>
+        <section>
+          <h2>Tops</h2>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='tops'/>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='tops'/>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='tops'/>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='tops'/>
+        </section>
+        <section>
+          <h2>Bottoms</h2>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='bottoms'/>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='bottoms'/>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='bottoms'/>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='bottoms'/>
+        </section>
+        <section>
+          <h2>Dresses</h2>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='bottoms'/>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='bottoms'/>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='bottoms'/>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='bottoms'/>
+        </section>
+        <section>
+          <h2>Shoes</h2>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='bottoms'/>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='bottoms'/>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='bottoms'/>
+          <input onClick={event => this.handleChange(event)} type='checkbox' name='bottoms'/>
+        </section>
+        <section>
+          <h2>Accessories</h2>
+          <input type='checkbox' name='bottoms'/>
+          <input type='checkbox' name='bottoms'/>
+          <input type='checkbox' name='bottoms'/>
+          <input type='checkbox' name='bottoms'/>
+        </section>
+      </form>
     )
   }
 }
 
 // Component Properties
-StyleSurvey.propTypes = {
-  dummyId: PropTypes.number.isRequired,
+// StyleSurvey.propTypes = {
+  // dummyId: PropTypes.number.isRequired,
   // someAction: PropTypes.func.isRequired,
-}
+// }
 
 // Component State
 // function styleSurveyReduxState(state) {
