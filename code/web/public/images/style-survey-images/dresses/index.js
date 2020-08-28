79f1f828e3c7d2e1595bb9a62ab682261721d63c
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-08-27 21:45:30 -0500

    Export style survey categories

---
+++ code/web/public/images/style-survey-images/dresses/index.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    style: 'casual',
+    image: './casual-dresses.jpg'
+  }, {
+    style: 'casual',
+    image: './Casual-dress-2.jpg'
+  }, {
+    style: 'country',
+    image: './country-dress-1.jpg'
+  }, {
+    style: 'country',
+    image: './country-dress-2.jpg'
+  }, {
+    style: 'grungy',
+    image: './grunge-dress-1.jpg'
+  }, {
+    style: 'grungy',
+    image: './grunge-dresses-2.jpg'
+  }, {
+    style: 'hip-hop',
+    image: './hiphop-dresses-1.jpg'
+  }, {
+    style: 'hip-hop',
+    image: './hiphop-dresses-2.jpg'
+  }, {
+    style: 'posh',
+    image: './posh-dress-1.jpg'
+  }, {
+    style: 'posh',
+    image: './posh-dress-2.jpg'
+  }, {
+    style: 'vintage',
+    image: './Vintage-Clothing-1.jpg'
+  }, {
+    style: 'vintage',
+    image: './vintage-dress-2.jpg'
+  }
+]
---
+++ code/web/public/images/style-survey-images/tops/index.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    style: 'casual',
+    image: './casual-top-1.jpg'
+  }, {
+    style: 'casual',
+    image: './casual-shirt-2.jpg'
+  }, {
+    style: 'country',
+    image: './country-top-1.jpg'
+  }, {
+    style: 'country',
+    image: './country-tops-2.jpg'
+  }, {
+    style: 'grungy',
+    image: './grungy-top-1.jpg'
+  }, {
+    style: 'grungy',
+    image: './grungy-top-2.jpg'
+  }, {
+    style: 'hip-hop',
+    image: './hiphop-top-1.jpg'
+  }, {
+    style: 'hip-hop',
+    image: './hiphop-top-2.jpg'
+  }, {
+    style: 'posh',
+    image: './posh-shoes-1.jpg'
+  }, {
+    style: 'posh',
+    image: './posh-shoes-2.jpg'
+  }, {
+    style: 'vintage',
+    image: './vintage-gent-shoes-1.jpg'
+  }, {
+    style: 'vintage',
+    image: './vintage-lady-shoes-1.jpg'
+  }
+]
